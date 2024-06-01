9d318596b99161e7f96fe4b5ad42ec15250ecfe6
MitomiTaisei <mton.marvel@gmail.com>
2024-06-01 14:04:45 +0900

    counterロジック削除

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,39 +10,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   final int _hogefuga = 100;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Flutter'),
+        title: const Text('Flutter Mitton App'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
             Text('$_hogefuga')
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => print('pressed'),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
