237d9a359fdf4d280feb8e09878b22f07aa2c289
MitomiTaisei <mton.marvel@gmail.com>
2024-06-02 14:17:23 +0900

    Nextボタンでランダムな英単語が表示されるよう変更

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,36 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:english_words/english_words.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyAppState extends ChangeNotifier{
+  var current = WordPair.random();
 
-  // final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  final int _hogefuga = 100;
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    const int hogefuga = 100;
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Flutter Mitton App'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: const Text('Flutter Mitton App', style: TextStyle(color: Colors.white)),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('$_hogefuga')
-          ],
+      body: Container(
+        color: Theme.of(context).colorScheme.primary,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('$hogefuga', style: TextStyle(fontSize: 30, color: Colors.white)),
+              Text(appState.current.asLowerCase, style: const TextStyle(fontSize: 30, color: Colors.white)),
+
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: const Text('Next')),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print('pressed'),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
