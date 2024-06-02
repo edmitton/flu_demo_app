fdba2d7fb2ec56ab4c807b5d9da1e4175c6ec9ea
MitomiTaisei <mton.marvel@gmail.com>
2024-06-02 20:09:01 +0900

    メインカードを別ウィジェットに切りだし, カラーテーマの調整

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,29 +16,72 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    const int hogefuga = 100;
+    var pair     = appState.current;
+    final theme  = Theme.of(context);
+    const String hogefuga = 'A random AWESOME idea:';
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: const Text('Flutter Mitton App', style: TextStyle(color: Colors.white)),
+        backgroundColor: theme.colorScheme.background,
+        title: Text(
+          'Flutter Mitton App',
+          style: TextStyle(color: theme.colorScheme.onBackground)
+        ),
       ),
       body: Container(
-        color: Theme.of(context).colorScheme.primary,
+        color: theme.colorScheme.background,
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Text('$hogefuga', style: TextStyle(fontSize: 30, color: Colors.white)),
-              Text(appState.current.asLowerCase, style: const TextStyle(fontSize: 30, color: Colors.white)),
+              Text(
+                hogefuga,
+                style: TextStyle(fontSize: 20, color: theme.colorScheme.onBackground)
+              ),
+              BigCard(pair: pair),
 
+              // MyAppStateのgetNext呼び出し
               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: theme.colorScheme.secondary,
+                  foregroundColor: theme.colorScheme.onSecondary,
+                ),
                 onPressed: () {
                   appState.getNext();
                 },
                 child: const Text('Next')),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displaySmall!.copyWith(color: theme.colorScheme.onPrimary);
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style
+          // style: TextStyle(
+          //   fontSize: 20,
+          //   color: theme.colorScheme.onPrimary,
+          // ),
         ),
       ),
     );
