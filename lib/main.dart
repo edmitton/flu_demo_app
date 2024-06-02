67572404c723da159f58455e39774bebcb945cb6
MitomiTaisei <mton.marvel@gmail.com>
2024-06-02 14:12:13 +0900

    ChangeNotifierProviderで返却、ColorSchemeの変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flu_demo_app/pages/home.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    const MyApp(
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,13 +11,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme(
+            brightness: Brightness.light,
+            primary: Colors.grey[900]!, // 主要な色
+            onPrimary: Colors.white, // 主要な色の上のテキスト色
+            secondary: Colors.grey[700]!, // 二次的な色
+            onSecondary: Colors.white, // 二次的な色の上のテキスト色
+            error: Colors.red[800]!, // エラー色
+            onError: Colors.white, // エラー色の上のテキスト色
+            background: Colors.grey[50]!, // 背景色
+            onBackground: Colors.black, // 背景色の上のテキスト色
+            surface: Colors.grey[100]!, // 表面色
+            onSurface: Colors.black,
+          ),
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
