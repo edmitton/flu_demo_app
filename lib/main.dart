f254a4fe0c087981cbaa0c48c34e1e60d02ea4e5
MitomiTaisei <mton.marvel@gmail.com>
2024-06-02 20:05:55 +0900

    オリジナルのカラーテーマを廃止, ダークモード対応

--- lib/main.dart
+++ lib/main.dart
@@ -14,23 +14,28 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Flutter Demo',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme(
-            brightness: Brightness.light,
-            primary: Colors.grey[900]!, // 主要な色
-            onPrimary: Colors.white, // 主要な色の上のテキスト色
-            secondary: Colors.grey[700]!, // 二次的な色
-            onSecondary: Colors.white, // 二次的な色の上のテキスト色
-            error: Colors.red[800]!, // エラー色
-            onError: Colors.white, // エラー色の上のテキスト色
-            background: Colors.grey[50]!, // 背景色
-            onBackground: Colors.black, // 背景色の上のテキスト色
-            surface: Colors.grey[100]!, // 表面色
-            onSurface: Colors.black,
-          ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+
+          // colorScheme: ColorScheme(
+          //   brightness: Brightness.light,
+          //   primary: Colors.grey[900]!, // 主要な色
+          //   onPrimary: Colors.white, // 主要な色の上のテキスト色
+          //   secondary: Colors.grey[700]!, // 二次的な色
+          //   onSecondary: Colors.white, // 二次的な色の上のテキスト色
+          //   error: Colors.red[800]!, // エラー色
+          //   onError: Colors.white, // エラー色の上のテキスト色
+          //   background: Colors.grey[50]!, // 背景色
+          //   onBackground: Colors.black, // 背景色の上のテキスト色
+          //   surface: Colors.grey[100]!, // 表面色
+          //   onSurface: Colors.black,
+          // ),
         ),
+        darkTheme: ThemeData.dark(useMaterial3: true),
+
+        themeMode: ThemeMode.system,
         home: const MyHomePage(),
       ),
     );
