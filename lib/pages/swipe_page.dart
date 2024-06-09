743c0a6a544ebb9fcabc318079731fd8e1bf564e
MitomiTaisei <mton.marvel@gmail.com>
2024-06-10 00:20:31 +0900

    swipeページ切り分け

---
+++ lib/pages/swipe_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SwipePage extends StatelessWidget {
+  const SwipePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Placeholder(),
+    );
+  }
+}
