aa89ac34536cf26fb0f5fe5d6c78899d3877fd9d
MitomiTaisei <mton.marvel@gmail.com>
2024-06-10 00:18:52 +0900

    page切り分けたのでhome.dartにimport

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:english_words/english_words.dart';
 import 'package:logger/logger.dart';
+
+// pages
+import 'generator_page.dart';
+import 'swipe_page.dart';
+import 'favorite_page.dart';
 
 class MyAppState extends ChangeNotifier{
   var current = WordPair.random();
@@ -94,130 +98,6 @@ class _MyHomePageState extends State<MyHomePage> {
             });
         })
         : null,
-    );
-  }
-}
-
-class GeneratorPage extends StatelessWidget {
-  const GeneratorPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair     = appState.current;
-    final theme  = Theme.of(context);
-
-    // NOTE: IconData型の変数iconを宣言し、Iconsクラスのiconを代入
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            BigCard(pair: pair),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFavorite();
-                  },
-                  icon: Icon(icon),
-                  label: const Text('Like'),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: theme.colorScheme.secondary,
-                    foregroundColor: theme.colorScheme.onSecondary,
-                  ),
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: const Text('Next')),
-              ],
-            ),
-            const SizedBox(height: 40),
-          ],
-        ),
-      );
-  }
-}
-
-class SwipePage extends StatelessWidget {
-  const SwipePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Placeholder(),
-    );
-  }
-}
-
-class FavoritePage extends StatelessWidget {
-  const FavoritePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return const Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: appState.favorites.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: const Icon(Icons.favorite),
-                  title: Text(appState.favorites[index].asLowerCase),
-                );
-              }
-            ),
-          ),
-        ],
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-
-        // NOTE: TextウィジェットでsemanticLabelプロパティは使用できなかった
-        child: Semantics(
-          label: "${pair.first} ${pair.second}",
-          child: Text(
-            pair.asLowerCase,
-            style: theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.onPrimary),
-          ),
-        ),
-      ),
     );
   }
 }
