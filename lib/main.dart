bc5ba2b51ce7fc92ce535d02ae2389460329e210
MitomiTaisei <mton.marvel@gmail.com>
2024-06-11 23:42:14 +0900

    mainのカラーテーマ調整

--- lib/main.dart
+++ lib/main.dart
@@ -20,23 +20,18 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-
-          // colorScheme: ColorScheme(
-          //   brightness: Brightness.light,
-          //   primary: Colors.grey[900]!, // 主要な色
-          //   onPrimary: Colors.white, // 主要な色の上のテキスト色
-          //   secondary: Colors.grey[700]!, // 二次的な色
-          //   onSecondary: Colors.white, // 二次的な色の上のテキスト色
-          //   error: Colors.red[800]!, // エラー色
-          //   onError: Colors.white, // エラー色の上のテキスト色
-          //   background: Colors.grey[50]!, // 背景色
-          //   onBackground: Colors.black, // 背景色の上のテキスト色
-          //   surface: Colors.grey[100]!, // 表面色
-          //   onSurface: Colors.black,
-          // ),
+          brightness: Brightness.light,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.lightBlue,
+          ),
         ),
-        darkTheme: ThemeData.dark(useMaterial3: true),
+        darkTheme: ThemeData(
+          useMaterial3: true,
+          brightness: Brightness.dark,
+          colorScheme: const ColorScheme.dark(
+            primary: Colors.lightBlue,
+          ),
+        ),
         themeMode: ThemeMode.system,
         home: const MyHomePage(),
       ),
