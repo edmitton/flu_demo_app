e44671732bc28c0a29ac849d900d41d94fbb7cd8
MitomiTaisei <mton.marvel@gmail.com>
2024-06-12 02:36:08 +0900

    Favoriteページリファクタ

--- lib/pages/favorite_page.dart
+++ lib/pages/favorite_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+// pages
 import 'home.dart';
 
 class FavoritePage extends StatelessWidget {
@@ -9,26 +11,29 @@ class FavoritePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
-    if (appState.favorites.isEmpty) {
-      return const Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: appState.favorites.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: const Icon(Icons.favorite),
-                title: Text(appState.favorites[index].asLowerCase),
-              );
-            }
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Favorites Page'),
+      ),
+      body: appState.favorites.isEmpty ?
+        const Center(
+          child: Text('No favorites yet.'),
+        )
+        : Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: appState.favorites.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: const Icon(Icons.favorite),
+                    title: Text(appState.favorites[index].asLowerCase),
+                  );
+                }
+              ),
+            ),
+          ],
         ),
-      ],
     );
   }
 }
