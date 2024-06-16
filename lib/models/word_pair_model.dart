203544d0a79d895313b267b5f57bb652cddf4588
MitomiTaisei <mton.marvel@gmail.com>
2024-06-17 02:29:11 +0900

    word_pairモデルを作成

---
+++ lib/models/word_pair_model.dart
@@ -0,0 +1,11 @@
+class WordPairModel {
+  final String first;
+  final String second;
+
+  WordPairModel(this.first, this.second);
+
+  @override
+  String toString() {
+    return '$first$second';
+  }
+}
