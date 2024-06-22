8db186de67dd007850f2a0e3cc34f6bac7975c2f
MitomiTaisei <mton.marvel@gmail.com>
2024-06-22 12:58:31 +0900

    ログイン方法をページ遷移ではなくwelcomページのアニメーションで実現

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
-class WelcomePage extends StatelessWidget{
-  final VoidCallback onSignUpPressed;
-  final VoidCallback onLoginPressed;
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key,}) : super(key: key);
 
-  const WelcomePage({
-    Key? key,
-    required this.onSignUpPressed,
-    required this.onLoginPressed,
-  }) : super(key: key);
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  bool showLoginSelection = true;
+
+  void toggleView() {
+    setState(() {
+      showLoginSelection = !showLoginSelection;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     appBar: AppBar(
+        leading: showLoginSelection
+            ? null
+            : IconButton(
+                icon: const Icon(Icons.arrow_back_ios),
+                onPressed: toggleView,
+              ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Center(
@@ -25,16 +40,13 @@ class WelcomePage extends StatelessWidget{
                 width: 100,
               ),
               const SizedBox(height: 32),
-              _buildButton(
-                context,
-                text: 'アカウントを作成する',
-                onPressed: onSignUpPressed,
-              ),
-              const SizedBox(height: 20),
-              _buildButton(
-                context,
-                text: 'ログイン',
-                onPressed: onLoginPressed,
+              AnimatedCrossFade(
+                firstChild: _buildSelectionButtons(),
+                secondChild: _buildLoginMethodButtons(),
+                crossFadeState: showLoginSelection
+                    ? CrossFadeState.showFirst
+                    : CrossFadeState.showSecond,
+                duration: const Duration(milliseconds: 500),
               ),
             ],
           ),
@@ -43,7 +55,78 @@ class WelcomePage extends StatelessWidget{
     );
   }
 
-  Widget _buildButton(BuildContext context, {
+  Widget _buildSelectionButtons() {
+    return Column(
+      key: const ValueKey(1),
+      children: [
+        _buildButton(
+          text: 'アカウントを作成する',
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Sign Up is not yet implemented.'),
+              ),
+            );
+          },
+        ),
+        const SizedBox(height: 20),
+        _buildButton(
+          text: 'Login',
+          onPressed: toggleView,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLoginMethodButtons() {
+    return Column(
+      key: const ValueKey(2),
+      children: [
+        const SizedBox(height: 32),
+        _buildButton(
+          text: 'Email',
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/login');
+          },
+        ),
+        const SizedBox(height: 20),
+        _buildButton(
+          text: 'Google',
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Google login is not yet implemented.'),
+              ),
+            );
+          },
+        ),
+        const SizedBox(height: 20),
+        _buildButton(
+          text: 'LINE',
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('LINE login is not yet implemented.'),
+              ),
+            );
+          },
+        ),
+        const SizedBox(height: 20),
+        _buildButton(
+          text: 'Apple',
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Apple login is not yet implemented.'),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildButton({
     required String text,
     required VoidCallback onPressed,
   }) {
